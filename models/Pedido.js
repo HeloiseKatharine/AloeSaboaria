6eeeddc9c5e87ae398b58a69b058f9457b74abc4
José Antônio <57533270+Josefreitas788@users.noreply.github.com>
2022-07-27 00:56:14 +0000

    att

--- models/Pedido.js
+++ models/Pedido.js
@@ -4,14 +4,10 @@ const Pedido = sequelize.define("pedido",{
   endereco: DataTypes.TEXT,
   frete : DataTypes.TEXT,
   valor_pedido : DataTypes.TEXT,
+  data : DataTypes.DATE
 });
 
 
 module.exports = Cliente;
 
 
-
-id SERIAL primary key ,
-endereco varchar(300) not null,
-frete numeric(10,2),
-valor_pedido numeric(10,2)
